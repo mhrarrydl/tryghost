8caecfe9da7d4d8025c5bae0e66b7b7663523d47
Fabien "egg" O'Carroll <fabien@allou.is>
2023-07-31 19:25:29 +0100

    Implemented a hacky in-memory HardcoreRouter
    We have the RoutingService which handles the grunt work of fetching data from
    storage (in this case some in-memory maps) - and we should then be able to
    replace that with a DB version.
    
    We base the HardcoreRouter off of the ProtoRouter, with a few adjustments.
    First we make `urlFor` async - this allows for async lookup from the DB in
    future. Second, we update the handling of the `getDataForUrl` return value, so
    that we can render templates. Both of these changes can be pulled into ProtoRouter.
    
    This will be useful because 1. future prototypes will be easier and 2. the
    differences between the prototypes will be smaller, and easier to comprehend.
    
    We have a weird populateRoutingService method - which in reality would be
    replaced by a database migration (to populate routes for resources) and maybe
    some kind of reading from a routes.yaml for the homepage stuff.

--- ghost/core/core/frontend/services/hardcore-router/HardcoreRouter.js
+++ ghost/core/core/frontend/services/hardcore-router/HardcoreRouter.js
@@ -1,4 +1,7 @@
 const debug = require('@tryghost/debug')('hardcore-router');
+const RoutingService = require('./RoutingService');
+const {checks} = require('../data');
+const rendering = require('../rendering');
 
 module.exports = class HardcoreRouter {
     constructor({express, config, urlUtils, api}) {
@@ -7,21 +10,235 @@ module.exports = class HardcoreRouter {
         this.config = config;
         this.urlUtils = urlUtils;
         this.api = api;
+
+        this.routingService = new RoutingService();
+        this.routingServiceReady = this.populateRoutingService();
     }
 
-    urlFor() {
-        // Lookup the URL from the storage
+    /**
+     * This would not be used in real life - this is just to make the prototype work
+     * Rather than sticking shit in the database RN - this will just populate the in-memory store.
+     * HAxxxxxxxXor
+     */
+    async populateRoutingService() {
+        const {collections} = await this.api.collections.browse();
+        const {tags} = await this.api.tags.browse();
+        const {posts} = await this.api.posts.browse();
+
+        const homepageURL = new URL(`/`, this.baseUrl());
+        await this.routingService.assignURL(homepageURL, {
+            type: 'template',
+            slug: 'homepage',
+            data: {}
+        });
+
+        for (const collection of collections) {
+            const url = new URL(`/${collection.slug}/`, this.baseUrl());
+            await this.routingService.assignURL(url, {
+                type: 'collection',
+                id: collection.id
+            });
+        }
+
+        for (const tag of tags) {
+            const url = new URL(`/tag/${tag.slug}/`, this.baseUrl());
+            await this.routingService.assignURL(url, {
+                type: 'tag',
+                id: tag.id
+            });
+        }
+
+        for (const post of posts) {
+            const url = new URL(`/post/${post.slug}/`, this.baseUrl());
+            await this.routingService.assignURL(url, {
+                type: 'post',
+                id: post.id
+            });
+        }
     }
 
+    baseUrl() {
+        return this.config.getSiteUrl();
+    }
+
+    async urlFor(data) {
+        await this.routingServiceReady;
+        if (checks.isPost(data)) {
+            return this.routingService.getURL({
+                id: data.id,
+                type: 'post'
+            });
+        } else if (checks.isNav(data)) {
+            // I don't really know what a "Nav" is
+            // I think we can just return the URL?
+            return this.urlUtils.urlJoin(this.baseUrl(), `/${data.url}/`);
+        } else if (checks.isTag(data)) {
+            return this.routingService.getURL({
+                id: data.id,
+                type: 'tag'
+            });
+        } else if (checks.isUser(data)) {
+            return this.routingService.getURL({
+                id: data.id,
+                type: 'author'
+            });
+        } else if (checks.isCollection(data)) {
+            return this.routingService.getURL({
+                id: data.id,
+                type: 'collection'
+            });
+        }
+    }
+
+    /**
+     * @param {URL} url
+     */
+    async getDataForUrl(url) {
+        const baseUrl = this.baseUrl();
+
+        const fullUrl = new URL(url, baseUrl);
+
+        const resource = await this.routingService.getResource(fullUrl);
+
+        if (!resource) {
+            return null;
+        }
+
+        if (resource.type === 'post') {
+            let {posts} = await this.api.posts.read({id: resource.id, formats: ['html']});
+            let post = posts[0];
+
+            return {
+                data: {post},
+                type: 'entry',
+                routerType: 'entry'
+            };
+        }
+
+        if (resource.type === 'tag') {
+            const apiOptions = {
+                limit: 15,
+                formats: ['html']
+            };
+
+            apiOptions.filter = `tag:${resource.id}`;
+
+            let posts = await this.api.posts.browse(apiOptions);
+
+            const data = posts;
+            data.pagination = posts.meta.pagination;
+
+            return {
+                data,
+                type: 'archive',
+                routerType: 'channel'
+            };
+        }
+
+        if (resource.type === 'author') {
+            const apiOptions = {
+                limit: 15,
+                formats: ['html']
+            };
+
+            apiOptions.filter = `author:${resource.id}`;
+
+            let posts = await this.api.posts.browse(apiOptions);
+
+            const data = posts;
+            data.pagination = posts.meta.pagination;
+
+            return {
+                data,
+                type: 'archive',
+                routerType: 'channel'
+            };
+        }
+
+        if (resource.type === 'collection') {
+            let posts = await this.api.posts.browse({
+                collection: resource.id,
+                formats: ['html']
+            });
+
+            const data = posts;
+            data.pagination = posts.meta.pagination;
+
+            return {
+                data,
+                type: 'archive',
+                routerType: 'channel'
+            };
+        }
+
+        if (resource.type === 'template') {
+            return {
+                data: {
+                    template: 'homepage'
+                },
+                type: 'static',
+                routerType: 'custom'
+            };
+        }
+
+        // TODO: resource.type === 'page'
+
+        return null;
+    }
+
+    /**
+     * @param {import('express').Request} req
+     * @param {import('express').Response} res
+     * @param {import('express').NextFunction} next
+     */
     async routeRequest(req, res, next) {
+        await this.routingServiceReady;
         let response = {
-            router: 'HardcoreRouter',
+            router: 'ProtoRouter',
             path: req.path
         };
 
-        // Routing logic here
+        let baseUrl = this.baseUrl();
 
-        res.status(501).send('<h1>Hardcore Stuff Not Implemented. Yet!</h1>');
+        const url = new URL(req.url, baseUrl);
+
+        const {data, type, routerType} = await this.getDataForUrl(url) || {};
+
+        if (!data) {
+            response.data = {};
+            response.type = 'unknown';
+            response.template = 'unknown';
+
+            const html = `<h1>Unknown route</h1><div><pre>${JSON.stringify(response, null, 2)}</pre></div><div><p>Visit <a href="${baseUrl}archive/">/archive/</a> to see a list of posts.</p></div>`;
+
+            res.status(404).send(html);
+        } else {
+            response.data = data;
+            res.routerOptions = {
+                type: routerType
+            };
+
+            if (data.posts) {
+                response.data.posts = await Promise.all(data.posts.map(async (post) => {
+                    return {
+                        ...post,
+                        url: await this.urlFor(post)
+                    };
+                }));
+            } else if (data.post) {
+                response.data.post = {
+                    ...data.post,
+                    url: await this.urlFor(data.post)
+                };
+            } else if (data.template) {
+                res.routerOptions.defaultTemplate = data.template;
+                res.routerOptions.templates = [data.template];
+            }
+
+            response.type = type;
+
+            rendering.renderer(req, res, response.data);
+        }
 
         debug('response', response);
         debug('routerOptions', res.routerOptions);
---
+++ ghost/core/core/frontend/services/hardcore-router/RoutingService.js
@@ -0,0 +1,85 @@
+/**
+ * @typedef {import('bson-objectid').default} ObjectID
+ */
+
+/**
+ * @typedef {Object} IDBasedResource
+ * @property {ObjectID} id - The ID of the resource.
+ * @property {'post' | 'page' | 'tag' | 'collection' | 'author'} type - The type of the resource.
+ */
+
+/**
+ * @typedef {Object} SlugBasedResource
+ * @property {string} slug - The slug of the resource.
+ * @property {'template'} type - The type of the resource.
+ * @property {{[key: string]: any}} data - Additional data for the resource.
+ */
+
+/**
+ * @typedef {(SlugBasedResource | IDBasedResource)} RoutingResource
+*/
+module.exports = class RoutingService {
+    constructor() {
+        this.urls = new Map();
+        this.resources = new Map();
+    }
+
+    /**
+     * @param {URL} url
+     * @returns {Promise<RoutingResource>}
+     */
+    async getResource(url) {
+        const resource = this.resources.get(url.toString());
+        if (resource) {
+            return resource;
+        }
+
+        return null;
+    }
+    /**
+     * @param {RoutingResource} resource
+     * @returns {Promise<URL>}
+     */
+    async getURL(resource) {
+        if ('id' in resource) {
+            return this.urls.get(resource.id.toString());
+        } else {
+            return this.urls.get(resource.slug);
+        }
+    }
+
+    /**
+     * @param {URL} url
+     * @param {RoutingResource} resource
+     * @returns {Promise<URL>}
+     */
+    async assignURL(url, resource) {
+        if (this.resources.has(url.toString())) {
+            return this.assignURL(this.getDifferentURL(url), resource);
+        }
+        if ('id' in resource) {
+            this.urls.set(resource.id.toString(), url);
+        } else {
+            this.urls.set(resource.slug, url);
+        }
+        this.resources.set(url.toString(), resource);
+        return url;
+    }
+
+    getDifferentURL(url) {
+        let path = url.pathname;
+
+        path = path.slice(0, -1); // Remove trailing slash
+
+        if (path.match(/-\d+$/)) {
+            const parts = path.split('-');
+            const number = parseInt(parts.pop(), 10);
+            parts.push(number + 1);
+            path = parts.join('-');
+        } else {
+            path += '-1';
+        }
+
+        return new URL(path + '/', this.baseURL); // Don't forget to add the trailing slash back
+    }
+};
