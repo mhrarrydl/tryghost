3e0d246608947d38b621c956149a045a665b7b86
Fabien "egg" O'Carroll <fabien@allou.is>
2023-08-09 11:16:27 +0100

    Removed need for host settings for Posts Cache
    Hoping this helps to fix the arch deployment

--- ghost/core/core/server/services/posts-public/service.js
+++ ghost/core/core/server/services/posts-public/service.js
@@ -7,32 +7,24 @@ class PostsPublicServiceWrapper {
 
         // Wire up all the dependencies
         const adapterManager = require('../adapter-manager');
-        const config = require('../../../shared/config');
         const EventAwareCacheWrapper = require('@tryghost/event-aware-cache-wrapper');
         const EventRegistry = require('../../lib/common/events');
 
-        let postsCache;
-        if (config.get('hostSettings:postsPublicCache:enabled')) {
-            const cache = adapterManager.getAdapter('cache:postsPublic');
-            postsCache = new EventAwareCacheWrapper({
-                cache: cache,
-                resetEvents: ['site.changed'],
-                eventRegistry: EventRegistry
-            });
-        }
+        const postsCache = new EventAwareCacheWrapper({
+            cache: adapterManager.getAdapter('cache:postsPublic'),
+            resetEvents: ['site.changed'],
+            eventRegistry: EventRegistry
+        });
 
-        let cache;
-        if (postsCache) {
-            // @NOTE: exposing cache through getter and setter to not loose the context of "this"
-            cache = {
-                get() {
-                    return postsCache.get(...arguments);
-                },
-                set() {
-                    return postsCache.set(...arguments);
-                }
-            };
-        }
+        // @NOTE: exposing cache through getter and setter to not loose the context of "this"
+        const cache = {
+            get() {
+                return postsCache.get(...arguments);
+            },
+            set() {
+                return postsCache.set(...arguments);
+            }
+        };
 
         this.api = {
             cache: cache
