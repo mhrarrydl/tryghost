1c65055910fbc321d5198e9c0336286862a7ec56
Naz <hi@nazavo.com>
2023-08-09 14:33:15 +0800

    Removed synthetic timestamp-based cache resets
    refs https://github.com/TryGhost/Arch/issues/5
    
    - Current event-aware cache wrapper has been using a timestamp as a way to create keys in Redis cache and reset them all at once. We are now moving on to the updated Redis adapter that supports "reset()" natively, so there's no need for synthetic resets.

--- ghost/event-aware-cache-wrapper/test/EventAwareCacheWrapper.test.js
+++ ghost/event-aware-cache-wrapper/test/EventAwareCacheWrapper.test.js
@@ -22,26 +22,23 @@ describe('EventAwareCacheWrapper', function () {
     describe('get', function () {
         it('calls a wrapped cache with extra key', async function () {
             const cache = new InMemoryCache();
-            const lastReset = Date.now();
             const wrapper = new EventAwareCacheWrapper({
-                cache: cache,
-                lastReset: lastReset
+                cache: cache
             });
 
             await wrapper.set('a', 'b');
             assert.equal(await wrapper.get('a'), 'b');
-            assert.equal(await cache.get(`${lastReset}:a`), 'b');
+            assert.equal(await cache.get('a'), 'b');
         });
     });
 
     describe('listens to reset events', function () {
         it('resets the cache when reset event is triggered', async function () {
             const cache = new InMemoryCache();
-            const lastReset = Date.now();
+
             const eventRegistry = new EventEmitter();
             const wrapper = new EventAwareCacheWrapper({
                 cache: cache,
-                lastReset: lastReset,
                 resetEvents: ['site.changed'],
                 eventRegistry: eventRegistry
             });
